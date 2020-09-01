9da9267c4126783d58004fe592518e7f1681fef5
Taras_Verhun <taras.verhun@dat.com>
2020-09-01 23:52:34 +0300

    chore: try deploy go-getting-started

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bin/go-tv
+web: bin/go-getting-started
--- main.go
+++ main.go
@@ -1,53 +1,29 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
 )
 
-type Page struct {
-	Materials []Material
-}
-
-type Material struct {
-	Title, Url string
-}
-
-var page = Page{
-	Materials: []Material{
-		{"http package", "https://golang.org/pkg/net/http/"},
-		{"Writing Web Applications", "https://golang.org/doc/articles/wiki/"},
-		{"Go by Example: HTTP Servers", "https://gobyexample.com/http-servers"},
-		{"Hello world HTTP server example", "https://yourbasic.org/golang/http-server-example/"},
-		{"How I write Go HTTP services after seven years", "https://medium.com/@matryer/how-i-write-go-http-services-after-seven-years-37c208122831"},
-		{"Gorilla web toolkit", "https://www.gorillatoolkit.org/pkg/mux"},
-	},
-}
-
-var templates = template.Must(template.ParseFiles("materials.html"))
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	materialsRoute := "/materials"
-	switch path := r.URL.Path; path {
-	case "/":
-		http.Redirect(w, r, materialsRoute, http.StatusFound)
-	case materialsRoute:
-		renderTemplate(w, path[1:], &page)
-	default:
-		http.NotFound(w, r)
-	}
-}
-
 func main() {
-	http.HandleFunc("/", handler)
+	port := os.Getenv("PORT")
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.Run(":" + port)
 }
